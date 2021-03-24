0b818d69cfb45316e4170ed470e1f3adae78fe59
Robert <redonebe@users.noreply.github.com>
2021-03-24 15:36:09 +0100

    T2

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -69,7 +69,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "GCRR Consulting V1,2" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "GCRR Consulting VSKR14" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
